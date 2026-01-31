c08339f22b07901c77696f9456210a3dcb5909ca
KaikeBeduTai-ao <kkbeduschitaiao@gmail.com>
2026-01-31 20:10:36 -0300

    Remove unused views and delete temporary HTML template

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,4 @@
 from django.shortcuts import render
-from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/home.html', context={'name': 'Kaike Taiao'})
-
-def contato(request):
-    return render(request, 'me-apague/temp.html')
-
-def sobre(request):
-    return render(request, 'recipes/sobre.html')
