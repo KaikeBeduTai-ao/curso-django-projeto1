02e1ea56cb87edb0b795c7657eecd5ab2559a8cf
KaikeBeduTai-ao <kkbeduschitaiao@gmail.com>
2026-01-31 18:53:08 -0300

    Refactor URL routing: move recipe URLs to a dedicated urls.py file

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,13 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-from recipes.views import home, contato, sobre
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home), # Home
-    path('contato/', contato), # Contato
-    path('sobre/', sobre), # Sobre
+    path('', include('recipes.urls')), # /
 ]
